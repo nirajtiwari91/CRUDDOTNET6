068dc52165537e2c05a850e5eda481a1db7b4893
HP <HP@DESKTOP-LGJ9AI7>
2022-07-24 17:19:48 +0530

    Add project files.

---
+++ CRUD_DOTNET6/Controllers/CategoryController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUD_DOTNET6.Models;
+using CRUD_DOTNET6.Data;
+using System.Collections.Generic;
+
+namespace CRUD_DOTNET6.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id==0)
+            {
+                return NotFound();
+            }
+
+            var CategoryFromDb = _db.Categories.Find(id);
+
+            if(CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(CategoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var CategoryFromDb = _db.Categories.Find(id);
+
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(CategoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+    }
+}
---
+++ CRUD_DOTNET6/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD_DOTNET6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_DOTNET6.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+
+        }
+
+        public  DbSet<Category> Categories { get; set; }
+    }
+}
